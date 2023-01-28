c2327082b7b4370c6a4f3c015fd53df9c81c048c
AceMouse <humle000@gmail.com>
2023-01-28 17:24:18 +0100

    represent state as row vector instead of col vector, implement one bit H, X, I and R_n gates, fix measure for complex and negative values

--- dense/sim.py
+++ dense/sim.py
@@ -1,6 +1,16 @@
 import numpy as np
-_0 = np.array([[1],[0]], dtype=complex)
-_1 = np.array([[0],[1]], dtype=complex)
+import math
+import cmath
+_0 = np.array([1,0], dtype=complex)
+_1 = np.array([0,1], dtype=complex)
+
+I = np.eye(2, dtype=complex)
+X = np.array([[0,1],[1,0]], dtype=complex)
+H = (1/math.sqrt(2))*np.array([[1,1],[1,-1]], dtype=complex)
+def R_n(n):
+    N = math.pow(2,n)
+    e = cmath.exp((2*math.pi*1j)/N)
+    return np.array([[1,0],[0,e]], dtype=complex)
 def parse_state(state_string):
     state = None
     for bit in state_string:    
@@ -19,23 +29,27 @@ def parse_state(state_string):
     return state
 
 def print_state(state, shorten=True, postfix=''):
-    size, _ = state.shape
+    (size,) = state.shape
     size -= 1
     print(f'{"#".rjust(len(str(size)))} {"state".ljust(size.bit_length()+2)} probability')
-    for i,s in enumerate(state[:,0]):
+    for i,s in enumerate(state):
         if not shorten or s != 0:
             print(f'{str(i).rjust(len(str(size)))} |{(f"{i:b}>".rjust(size.bit_length()+1,"0")).ljust(5)} {s}{postfix}')
 
 def measure(state):
-    return np.square(state)
+    return np.abs(np.square(state, dtype=complex))
 
 def print_measurement(state, shorten=True):
-    print_state(measure(state).real*100, shorten, postfix='%')
+    print_state(np.around(measure(state)*100,decimals=1), shorten, postfix='%')
 
+for n in range(10):
+    state = np.dot(parse_state("1"),R_n(n))
+    print_measurement(state)
+    print_state(state)
 
-
-
+'''
 state_string = input()
 state = parse_state(state_string)
 print_state(state)
 print_measurement(state)
+'''
