52a71d211a7939900da0d83f39ab13d39d65e222
AceMouse <humle000@gmail.com>
2023-01-29 23:18:28 +0100

    add compiler to compile cirquit to file with instructions

---
+++ compile/compiler.py
@@ -0,0 +1,54 @@
+import sys
+import re
+import os
+def T(l):
+    return [list(x) for x in zip(*l)]
+
+def compile(path):
+    tokens = []
+    with open(path, 'r') as cirq:
+        lines = cirq.readlines()
+        for i,line in enumerate(lines):
+            tokens.append(re.findall('..',line))
+    instructions = []
+    for col in T(tokens):
+        c = None
+        t = None
+        U = None
+        for qubit, token in enumerate(col):
+            if token == '--':
+                continue
+            if token == 'C-':
+                c = qubit
+                continue
+            m = re.search('R\d',token)
+            if not m is None:
+                U = m.group()
+                t = qubit
+                continue
+            m = re.search('[HX]-',token)
+            if not m is None:
+                U = m.group()[0]
+                t = qubit
+                continue
+        if U is None: 
+            continue
+        if not c is None:
+            instructions.append('c')
+        instructions.append(U)
+        if not c is None:
+            instructions.append(c+1)
+        instructions.append(t+1)
+
+    out_path = path + '.out'
+    try:
+        os.remove(out_path)
+    except OSError:
+        pass
+
+    with open(out_path, 'a') as out:
+        for instr in instructions:
+            out.write(f'{instr}\n')
+        
+
+compile(sys.argv[1])
