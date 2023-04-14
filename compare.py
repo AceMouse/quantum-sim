aea66f41397b91de924745da6b88950e78c924ee
AceMouse <humle000@gmail.com>
2023-04-14 20:27:14 +0200

    fix compare

--- compare.py
+++ compare.py
@@ -2,35 +2,35 @@ from dense.sim import interpret as dinterpret
 from tensor_network.sim import interpret as tinterpret
 import numpy as np
 matrix = dinterpret("circuits/QFT8.out", silent=True)
-mpos = [(tinterpret("circuits/QFT8_g_l.out",silent=True,max_bond=16),1) ] 
-#mpos = [(tinterpret("circuits/QFT2.out",silent=True,max_bond=i),i) for i in range(2,33)] 
-#print(dstate)
-#print(tstate.to_dense())
 maxes = []
-for mpo,c in mpos:
-    diffs = np.array([(x-y) for x,y in zip(matrix.flatten(), mpo.to_dense().flatten())])
-    maxes += [(np.sum(diffs*diffs.conjugate()).real,c)] 
+for c in range(16,64):
+    mpo = tinterpret("circuits/QFT8_g_l.out",silent=True,max_bond=c).to_dense()
+    diffs = np.array([(x-y) for x,y in zip(matrix.flatten(), mpo.flatten())])
+    m=np.sum(diffs*diffs.conjugate()).real
+    print(f'max_bond({c}): {m}')
+    maxes += [(m,c)] 
+    if m < 5e-25:
+        break
 
-mpos = [(tinterpret("circuits/QFT8_g_l.out",silent=True,cutoff=10**-10),1) ] 
-#mpos = [(tinterpret("circuits/QFT8_g_l.out", silent=True,cutoff=1/(2**i)),1/(2**i)) for i in range(10,30)]
-#print(dstate)
-#print(tstate.to_dense())
 cutoffs = []
-for mpo,c in mpos:
-    diffs = np.array([(x-y) for x,y in zip(matrix.flatten(), mpo.to_dense().flatten())])
-    cutoffs += [(np.sum(diffs*diffs.conjugate()).real,c)] 
+for i in range(16,64):
+    c = 1/(2**i)
+    mpo = tinterpret("circuits/QFT8_g_l.out",silent=True,cutoff=c).to_dense()
+    diffs = np.array([(x-y) for x,y in zip(matrix.flatten(), mpo.flatten())])
+    m=np.sum(diffs*diffs.conjugate()).real
+    print(f'cutoff({c}: {m})')
+    cutoffs += [(m,c)] 
+    if m < 5e-25:
+        break
 
-print(cutoffs)
-print(maxes)
-'''
 import matplotlib.pyplot as plt
 
 plt.plot([c for m,c in maxes], [m for m,c in maxes])
   
 plt.xlabel('max_bond')
 plt.ylabel('diff')
-plt.semilogx()
-plt.semilogy()
+#plt.semilogx()
+#plt.semilogy()
 plt.title('max_bond')
   
 plt.show()
@@ -45,8 +45,3 @@ plt.title('cutoff')
   
 plt.show()
 
-for m,c in maxes:
-    print(f'{m:.3f}: max_bond({c})')
-for m,c in cutoffs:
-    print(f'{m:.3f}: cutoff({c})')
-'''
